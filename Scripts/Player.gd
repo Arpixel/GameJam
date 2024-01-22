cf593ea4796012fcfbad9aed5dacdc141935f0b9
Eka <mycraft323@gmail.com>
2024-01-22 20:32:57 +0300

    KarakterKodu

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,45 +1,35 @@
 extends KinematicBody2D
 
-var direction = Vector2()
+
 var velocity = Vector2()
-var speed = 100
-var gravity = 1000
-var jump_strength = 220
+var speed = 150
+var gravity = 15
+var jump_strength = -270
+var dir = 0
 
-func _ready():
-	pass
 
-func _physics_process(delta):
-	process_input()
-	update_movement(delta)
-	player_animation()
 
-func process_input():
-	direction = Vector2()
+func _process(delta):
 	if Input.is_action_pressed("D"):
-		direction.x += 1
-	if Input.is_action_pressed("A"):
-		direction.x -= 1
-
-	if is_on_floor() and Input.is_action_just_pressed("Space"):
-		start_jump()
-		return
-
-func start_jump():
-	velocity.y = -jump_strength
-
-func update_movement(delta):
-	velocity.x = direction.x * speed
-	velocity.y += gravity * delta
-
-	velocity = move_and_slide(velocity, Vector2.UP)
-
-func player_animation():
-	if is_on_floor():
-		if direction.x != 0:
-			$AnimatedSprite.flip_h = (direction.x < 0)
-			$AnimatedSprite.play("Walk")
-		else:
-			$AnimatedSprite.play("Idle")
+		dir = 1
 	else:
-		$AnimatedSprite.play("Jump")
+		dir = 0
+	if Input.is_action_pressed("A"):
+		dir = -1
+	if Input.is_action_pressed("ui_select") and is_on_floor():
+		velocity.y = jump_strength
+	
+	velocity.x = speed * dir
+	velocity.y += gravity
+	move_and_slide(velocity, Vector2.UP)
+
+
+func UpdateAnimation():
+	if dir == 1:
+		pass
+	if dir == 0:
+		$AnimatedSprite.play("default")
+	if dir == -1:
+		pass
+
+
