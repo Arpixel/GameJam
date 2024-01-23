2c9dd1a47148984593161ca912202e12e548b32b
Slowerka <teamslowerka@outlook.com>
2024-01-23 21:16:35 +0300

    Hucre Cikis Update

---
+++ Scripts/Level 0.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var Hucre_Kapisi_Aktif = true
+onready var Hucre_Kapisi = $CevreTasarim/Hucre_Kapisi/Area2D
+
+func _process(_delta):
+	if Input.is_action_just_pressed("E") and Hucre_Kapisi_Aktif == true:
+		get_tree().change_scene("res://Hucre.tscn")
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		Hucre_Kapisi_Aktif = true
+		$Player/Player/ActionText.text = "press e to enter"
+
+
+func _on_Area2D_body_exited(body):
+	if body.name == "Player":
+		Hucre_Kapisi_Aktif = false
+		$Player/Player/ActionText.text = ""
