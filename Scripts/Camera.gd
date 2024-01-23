fc58be6302b0d80588e0111bd3e69795e02a5659
Slowerka <teamslowerka@outlook.com>
2024-01-23 19:32:27 +0300

    Alarm System Update

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,8 +1,6 @@
 extends StaticBody2D
 
 
-var alarm = false
-
 onready var raycast_01 = $"Sprite 00/RayCast2D 01"
 onready var raycast_00 = $"Sprite 00/RayCast2D 00"
 
@@ -10,7 +8,7 @@ onready var anim_player = $AnimationPlayer
 
 
 func _process(_delta):
-	if alarm == false:
+	if Global.alarm_system == false:
 		if raycast_01.is_colliding() or raycast_00.is_colliding():
 			anim_player.play("AlarmOnAnim")
-			alarm = true
+			Global.alarm_system = true
