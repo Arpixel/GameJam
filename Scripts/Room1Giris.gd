34dddc687913302dd4b3b43250b399a7319065e1
Plewr32B <aliefek2016@gmail.com>
2024-01-23 20:41:53 +0300

    Kapi

--- Scripts/Room1Giris.gd
+++ Scripts/Room1Giris.gd
@@ -3,14 +3,8 @@ extends Area2D
 
 func _on_Room1Giris_body_entered(body):
 	if body.name == "Player":
-		$Sprite2.visible = true
 		if Global.key == true:
 			$Button.visible = true
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Room1YemekOda.tscn")
-
-
-func _on_Room1Giris_body_exited(body: Node) -> void:
-	if body.name == "Player":
-		$Sprite2.visible = false
