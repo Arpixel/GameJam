93596050a0c2f8fe4926cc9cbf04658af81561b3
Slowerka <teamslowerka@outlook.com>
2024-01-24 11:03:30 +0300

    Removing Cube Adding Vent

--- Scripts/Cube 00.gd
+++ Scripts/Cube 00.gd
@@ -1,14 +1,17 @@
-extends StaticBody2D
+extends Node2D
 
 
 var Aktif = false
+onready var AnimSprite = $"AnimatedSprite 00"
 
 
 func _process(_delta):
 	if Input.is_action_just_pressed("E") and Aktif == true and Global.Player_Crouch == false:
 		Global.Player_Crouch = true
+		AnimSprite.play("Hide")
 	elif Input.is_action_just_pressed("E") and Global.Player_Crouch == true:
 		Global.Player_Crouch = false
+		AnimSprite.play("default")
 
 
 func _on_Area2D_body_entered(body):
