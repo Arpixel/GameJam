52ed4503a54bffd28ade00d106685400f91d46bb
Slowerka <teamslowerka@outlook.com>
2024-01-25 14:25:46 +0300

    Main Menu Updated

--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -1,12 +1,21 @@
 extends Control
 
+
+onready var toilet = $Objects/Toilet/Sprite
+onready var character = $Objects/Character
+
+
 func _ready():
 	Global.menu_engelle = true
+	toilet.play("Idle")
+	character.play("Idle")
+
 
 func _on_Button_pressed():
 	Global.menu_engelle = false
 	Global.menukapa = true
 	get_tree().change_scene("res://Scenes/Hucre.tscn")
+
 
 func _on_Button2_pressed():
 	get_tree().quit()
@@ -17,3 +26,7 @@ func _on_Button3_pressed():
 	Global.Posy = -10
 	Global.Save_Game()
 	get_tree().change_scene("res://Scenes/Hucre.tscn")
+
+
+func _on_Button_button_up():
+	toilet.play("Flush")
